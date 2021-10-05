6de239fa6f306b466a1a07abe65efe7e3da0bebd
unknown <>
2021-10-04 23:03:18 -0400

    Uses for loop instead

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -1,3 +1,8 @@
+# The Nameless Team: Patrick Ging, Eric Guo, Hebe Huang
+# SoftDev
+# Occupation picker using Flask
+# 2021-10-04
+
 from flask import Flask
 import csv
 import random
@@ -14,13 +19,16 @@ def index():
             next(csv_reader)
             sum = 0.0
 
+            jobTable = "<table> <tr> <th> Possible Jobs </th> </tr>"
+
             for value in csv_reader:
-                            if(value[0] == "Total"):
-                                    continue
-                            sum = sum + (10 * float(value[1])) #adding the values
-                            job_and_prob[sum] = value[0]
-
-
+                if(value[0] == "Total"):
+                        continue
+                sum = sum + (10 * float(value[1])) #adding the values
+                job_and_prob[sum] = value[0]
+                jobTable += "\n<tr> <td> " + value[0] + " </tr></td>"
+            
+            jobTable += "</table>"
 
 
             randNum = random.randrange(998) #random number
@@ -28,31 +36,7 @@ def index():
                 if(percent >= randNum): #if it's below the value that means it's in between it and the value before it - elsewise we know it's above the value so we don't actually need another logic operator
                         returnstring = '<h2>The Nameless Team: Patrick Ging, Eric Guo, Hebe Huang</h2> </hr>'
                         returnstring += '<h2>Your job is ' +  job_and_prob.get(percent) + '</h2></br>'
-                        returnstring += "<table> <tr> <th> Possible Jobs </th> </tr>";
-                        returnstring += "<tr> <td> Management </tr></td>"
-                        returnstring += "<tr> <td> Buisness and Financial operations </tr></td>"
-                        returnstring += "<tr> <td> Computer and Mathematical </tr></td>"
-                        returnstring += "<tr> <td> Architecture and Engineering </tr></td>"
-                        returnstring += "<tr> <td> Life, Physical and Social Science </tr></td>"
-                        returnstring += "<tr> <td> Community and Social Service </tr></td>"
-                        returnstring += "<tr> <td> Legal </tr></td>"
-                        returnstring += "<tr> <td> Education, training and library </tr></td>"
-                        returnstring += "<tr> <td> Arts, design, entertainment, sports and media </tr></td>"
-                        returnstring += "<tr> <td> Healthcare practioners and technical </tr></td>"
-                        returnstring += "<tr> <td> Healthcare support </tr></td>"
-                        returnstring += "<tr> <td> Protective service </tr></td>"
-                        returnstring += "<tr> <td> Food preparation and serving </tr></td>"
-                        returnstring += "<tr> <td> Building and grounds cleaning and maintenance </tr></td>"
-                        returnstring += "<tr> <td> Personal care and service </tr></td>"
-                        returnstring += "<tr> <td> Sales </tr></td>"
-                        returnstring += "<tr> <td> Office and administrative support </tr></td>"
-                        returnstring += "<tr> <td> Farming, fishing and forestry </tr></td>"
-                        returnstring += "<tr> <td> Construction and extraction </tr></td>"
-                        returnstring += "<tr> <td> Installation, maintenance and repair </tr></td>"
-                        returnstring += "<tr> <td> Production </tr></td>"
-                        returnstring += "<tr> <td> Transportation and material moving </tr></td>"
-                        returnstring += "</table>"
-                        return returnstring
+                        return returnstring + jobTable
 
 if __name__ == "__main__":
     app.debug = True
