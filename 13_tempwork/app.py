8c054cbf469fab365dce07a58a9a7ee722b11dc1
EGuo2004 <eguo20@stuy.edu>
2021-10-12 09:17:58 -0400

    Updates code

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -4,7 +4,7 @@
 #10/8/21
 import csv
 import random
-from flask import Flask
+from flask import Flask, render_template
 
 # Declaration of dictionary to store CSV File contents
 finalDict = {}
@@ -13,14 +13,14 @@ app = Flask(__name__)
 
 # Read in CSV file
 def fileReader():
-    with open('occupations.csv', newline='') as csvfile:
+    with open('data/occupations.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             finalDict[row['Job Class']] = float(row['Percentage'])
 
 # Choose random job from dictionary
 def randomJob(reader):
-    x = random.randint(0, 1000)
+    x = random.randint(0, 998)
     y = 0
     for i in reader:
         y += reader[i] * 10
@@ -32,9 +32,13 @@ def randomJob(reader):
 @app.route("/occupyflaskst")
 def displayApp():
     fileReader()
-    return ("Team Team - Alif Abdullah, Eric Guo, Shadman Rakib Period 2 <br><br>List of occupations: <br>" +
+    return render_template('tablified.html',heading="Alif Abdullah SoftDev K13 - To create a flask app that randomly chooses and displays an occupation based on a template 10/8/21",
+                           title="Weighted Random Occupations Chooser",tnpg="Team Team : Alif Abdullah, Eric Guo, Shadman Rakib Period 2",collection=finalDict,chosenJob=("Chosen Job: " + str(randomJob(finalDict))))
+    
+    '''return ("Team Team - Alif Abdullah, Eric Guo, Shadman Rakib Period 2 <br><br>List of occupations: <br>" +
         str(list(finalDict.keys())[:len(list(finalDict.keys()))-1]) +
         "<br><br>Chosen Occupation: " + str(randomJob(finalDict)))
+    '''
 
 app.debug = True
 app.run()
