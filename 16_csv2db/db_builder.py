802dbc7c0b615ceb4d22eb3420b04ecc4784d332
Eric Guo <72960536+EGuo2004@users.noreply.github.com>
2021-10-25 09:14:24 -0400

    Update db_builder.py

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -13,38 +13,33 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
-# Read in courses.csv
+
+# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+
 with open('courses.csv', newline='') as csvfile0:
-    #reads courses.csv
     readerC = csv.DictReader(csvfile0)
-    # test SQL stmt in sqlite3 shell, save as string + run SQL statement
-    command = ""
-    c.execute(command)
-     #Creates a table named courses if it doesn't already exist, the columns are a TEXT column code, and two INTEGER columns mark and id
+    command = ""          # test SQL stmt in sqlite3 shell, save as string
+    c.execute(command)    # run SQL statement
     c.execute("CREATE TABLE IF NOT EXISTS courses (code TEXT, mark INTEGER, id INTEGER);")
-    #for every row in the file read, it will make a new row inserting information in the appropriate columns.
     for row in readerC:
         c.execute("INSERT INTO courses VALUES (\"" +row['code']+ "\"," +row['mark']+"," +row['id']+ ");")
-
-# Read in students.csv
 with open('students.csv', newline='') as csvfile1:
     readerS = csv.DictReader(csvfile1)
-    c.execute(command)
+    c.execute(command)    # run SQL statement
     c.execute("CREATE TABLE IF NOT EXISTS students (name TEXT, age INTEGER, id INTEGER);")
     for row in readerS:
         c.execute("INSERT INTO students VALUES (\"" +row['name']+ "\"," +row['age']+"," +row['id']+ ");")
 
 #TESTING courses import:
+c.execute("SELECT * FROM courses")
 colnames = c.description
 for line in colnames:
     print(line[0])
-c.execute("SELECT * FROM courses")
 rows = c.fetchall()
 for row in rows:
     print(row)
 
 print("\n\n")
-
 #TESTING students import:
 c.execute("SELECT * FROM students")
 colnames = c.description
@@ -53,7 +48,6 @@ for line in colnames:
 rows = c.fetchall()
 for row in rows:
     print(row)
-
 #==========================================================
 
 db.commit() #save changes
