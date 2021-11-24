87a5f7ad312b1aaed7799a76bb55f5c716528555
EGuo2004 <hengguo2004@gmail.com>
2021-11-23 22:28:19 -0500

    Done

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,19 +1,29 @@
 # Softlocked - Eric Guo and Michael Borczuk
 # SoftDev
-# Oct 2021
+# K19 - Nasa RESTApi Stuff
+# 2021-11-23
+# time spent: 0.6 hours
 
 from flask import Flask, render_template
-import urllib3
+import urllib
 import json
+import os
 
 app = Flask(__name__) 
 
 @app.route("/") 
 def hello_world():
-    print(__name__) 
-    picture = urllib3.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DzW2D4l1DakGW9bo7dheF10L7evZVr2YyzkOIvCi")
-    print (picture);
-    return render_template("main.html", pic="")
+    print(__name__)
+    script_dir = os.path.dirname(__file__) # avoid filenotfound error for key file
+    print(script_dir)
+    path = os.path.join(script_dir, "key_nasa.txt") # construct path to key file
+    f = open(path) 
+    key = f.read()
+    response = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=" + key) # join key with base url
+    print(response)
+    json_stuff = json.loads(response.read())  
+    print(json_stuff)
+    return render_template("main.html", pic=json_stuff["url"], expl=json_stuff["title"])
 
 app.run()  
 
